ea6ac2337ce3863cfa133af8d748c38eb2fc860f
Renan Martins <email": null>
2016-05-05 15:43:50 -0700

    Add Add bcrypt to User model, setup Gemfiles & Env

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'rake'
 gem 'shotgun'
 
 gem 'bcrypt'
+gem 'faker'
 
 group :test do
   gem 'shoulda-matchers'
@@ -25,5 +26,5 @@ end
 
 group :test, :development do
   gem 'factory_girl'
-  gem 'faker'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  include BCrypt
   has_many :tweets
 
   validates :handle, uniqueness: true, presence: true
@@ -6,4 +7,12 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true
   validates :password_hash, presence: true
 
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
