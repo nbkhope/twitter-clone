f536dd2ff6970dd9def280c3bcbd899843e52d4b
Renan Martins <email": null>
2016-05-05 15:10:44 -0700

    User and Tweet model along with migration to create User and Tweet table

---
+++ db/migrate/20160505150353_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :handle, null: false
+      t.string :password_hash, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
