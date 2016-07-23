21a3ef61627234b87b6e01aca2644b42922f9cca
nbkhope <ryumemaru@yahoo.com>
2016-07-23 13:36:10 -0700

    Add uniqueness tests for handle and email in user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../spec_helper'
 
 describe User do
-  let(:user) do
-    User.new(
+  let!(:user) do
+    User.create!(
       name: "Some Person",
       email: "some_person123456@mail.com",
       handle: "someperson123456",
@@ -23,4 +23,28 @@ describe User do
     expect(user.password).not_to be_nil
     expect(user.password).not_to eq ""
   end
+
+  it "should a have a unique handle" do
+    expect do
+      User.create!(
+        name: "Jerry Somebody",
+        email: "some_person654321@mail.com",
+        handle: "someperson123456",
+        password: "654321"
+      )
+    end.to raise_error ActiveRecord::RecordInvalid
+  end
+
+  it "should a have a unique email" do
+    expect do
+      User.create!(
+        name: "Jerry Somebody",
+        email: "some_person123456@mail.com",
+        handle: "someperson654321",
+        password: "654321"
+      )
+    end.to raise_error ActiveRecord::RecordInvalid
+  end
+
+
 end
